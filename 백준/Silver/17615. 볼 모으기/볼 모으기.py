55d55ce25d6b3d419c49b68ff2002e1847a1fe6b
재민 <111514410+WooJJam@users.noreply.github.com>
2023-12-04 16:53:14 +0900

    [Silver I] Title: 볼 모으기, Time: 56 ms, Memory: 32584 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17615. 볼 모으기/볼 모으기.py
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.py
@@ -1,15 +1,15 @@
 import sys
 
-N = int(sys.stdin.readline())
-ball = sys.stdin.readline().strip()
-answer=[]
+input = lambda: sys.stdin.readline().rstrip()
+def move_balls(type_of_ball_to_move,s):
+    s=s.lstrip(type_of_ball_to_move)
+    return s.count(type_of_ball_to_move)
 
-remain = ball.rstrip('R')
-answer.append(remain.count('R'))
-reamin = ball.rstrip('B')
-answer.append(remain.count('B'))
-remain = ball.lstrip('R')
-answer.append(remain.count('R'))
-remain = ball.lstrip('B')
-answer.append(remain.count('B'))
-print(min(answer))
+n = int(input())
+s=input()
+# To consider: red to left, red to right, blue to left, blue to right
+print(min(
+    move_balls("R",s),
+    move_balls("R",s[::-1]),
+    move_balls("B",s),
+    move_balls("B",s[::-1])))
