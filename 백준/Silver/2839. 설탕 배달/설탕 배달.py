967201cf2e9c4ef50cea3967459dd6009c13f796
재민 <111514410+WooJJam@users.noreply.github.com>
2023-12-11 16:31:09 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,20 +1,14 @@
-import sys
-N = int(sys.stdin.readline())
-count = 0
-if N % 5 == 0:
-    print(N // 5)
-else:
-    while(N > 0):
-        N -= 3
-        count += 1
-        if N % 5 == 0:
-            print(count + N // 5)
-            break
-        elif N == 1 or N == 2:
-            print(-1)
-            break
-        elif N == 0:
-            print(count)
-            break
-    else:
-        print(-1)
+def dynamic(n):
+    for i in range(6, n+1):
+        if n % 5 == 0:
+            dp[i] = dp[i-5] + 1
+        elif dp[i-3] <= 0 or dp[i-5] <= 0:
+            dp[i] = 1 + (max(dp[i-3], dp[i-5]))
+        else:
+            dp[i] = 1 + (min(dp[i-3], dp[i-5]))
+    return dp[n]
+x = int(input())
+dp = [-1] * 5001
+dp[3] = dp[5] = 1
+answer = dynamic(x)
+print(-1 if answer <= 0 else answer)
