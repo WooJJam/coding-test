ee389c2734ddb9bc34dd763e409cca6644c1a622
WooJJam <jaemin5548@naver.com>
2023-12-26 22:11:20 +0900

    [Feat] Dijkstra + Priority Queue

--- algorithms/Dijkstra.py
+++ algorithms/Dijkstra.py
@@ -3,8 +3,10 @@
 # O(V)번에 걸쳐서 선형 탐색
 # 시간복잡도: O(V^2)
 # 즉, 전체 노드의 개수가 5,000개 이하라면 해결 가능
+# 우선순위큐를 사용하여 성능을 개선할 수 있음
 
 import sys 
+import heapq
 input = sys.stdin.readline()
 INF = int(1e9)
 
@@ -29,34 +31,28 @@ for _ in range(m):
     # a번 노드에서 b번 노드로 가는 비용이 c라는 의미
     graph[a].append((b,c))
 
-# 방문하지 않은 노드 중에서, 가장 최단 거리가 짧은 노드의 번호를 반환
-def get_smallest_node():
-    min_value = INF
-    index = 0 # 가장 최단 거리가 짧은 노드(index)
-    for i in range(1, n+1):
-        if distance[i] < min_value and not visited[i]:
-            min_value = distance[i]
-            index = i
-    return index
-
 def dijkstra(start):
-    # 시작 노드에 대해서 초기화
+    q = []
+    # 시작 노드로 가기 위한 최단 경로는 0으로 설정하여, 큐에 삽입
+    heapq.heappush(q, (0,start))
     distance[start] = 0
     visited[start] = True
+    while q:
+        # 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
+        dist, now = heapq.heappop(q)
 
-    for j in graph[start]:
-        distance[j[0]] = j[1]
-    # 시작 노드를 제외한 전체 n - 1개의 노드에 대해 반복
-    for i in range(n-1):
-        # 현재 최단 거리가 가장 짧은 노드를 꺼내서, 방문 처리
-        now = get_smallest_node()
-        visited[now] = True
-        # 현재 노드와 연결된 다른 노드를 확인
-        for j in graph[now]:
-            cost = distance[now] + j[1]
-            # 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
-            if cost < distance[j[0]]:
-                distance[j[0]] = cost
+        # 현재 노드가 이미 처리된 적이 있는 노드라면 무시
+        if distance[now] < dist:
+            continue
+
+        # 현재 노드와 연결된 다른 인접한 노드들을 확인
+        for i in graph[now]:
+            cost = dist + i[1]
+
+            # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
 
 # 다익스트라 알고리즘을 수행
 dijkstra(start)
