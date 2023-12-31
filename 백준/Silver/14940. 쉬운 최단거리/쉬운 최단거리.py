5307dc53deee31c1184832096a1d84b06844068d
재민 <111514410+WooJJam@users.noreply.github.com>
2023-12-31 17:25:10 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 568 ms, Memory: 40088 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -6,7 +6,6 @@ dist = 0
 N, M = map(int, input().split())
 visited = [[False for _ in range(M)] for _ in range(N)]
 arr = []
-res = [[-1] *M for _ in range(N)]
 dx = [-1,0,1,0]
 dy = [0,1,0,-1]
 queue = deque([])
@@ -22,33 +21,28 @@ for i in range(N):
     if not repeat:
         break
 
-def bfs(x,y):
-    queue.append([x,y])
+def bfs(x,y,dist):
+    queue.append([x,y,dist])
     visited[x][y] = True
-    res[x][y] = 0
-
     while queue:
-        x, y = queue.popleft()
-
+        x, y, dist = queue.popleft()
+        if arr[x][y]:
+            arr[x][y] = dist
+        
         for i in range(4):
             nextX = x + dx[i]
             nextY = y + dy[i]
 
-            if 0<= nextX < N and 0<= nextY < M and not visited[nextX][nextY]:
-                if arr[nextX][nextY] == 0 :
-                    visited[nextX][nextY] = True
-                    res[nextX][nextY] = 0
-                elif arr[nextX][nextY] == 1:
-                    visited[nextX][nextY] = True
-                    res[nextX][nextY] = res[x][y] + 1
-                    queue.append([nextX, nextY])
-    
-bfs(x,y)
+            if 0<= nextX < N and 0<= nextY < M and arr[nextX][nextY] != 0 and not visited[nextX][nextY]:
+                queue.append([nextX, nextY, dist+1])
+                visited[nextX][nextY] = True
+
+bfs(x,y, dist)
 
 for i in range(N):
     for j in range(M):
-        if arr[i][j] == 0:
-            print(0, end=" ")
-        else:
-            print(res[i][j], end=" ")
+        if arr[i][j] == 1 and not visited[i][j]:
+            arr[i][j] = -1
+            print(arr[i][j], end=' ')
+        else: print(arr[i][j], end=' ')
     print()
