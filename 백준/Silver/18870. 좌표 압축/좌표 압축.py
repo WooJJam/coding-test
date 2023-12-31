ead957e8f9cbf1c7f9e5e49da3dfbe4bd93ef918
재민 <111514410+WooJJam@users.noreply.github.com>
2023-12-31 15:33:51 +0900

    [Silver II] Title: 좌표 압축, Time: 1864 ms, Memory: 143264 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,10 +1,8 @@
 import sys
-from bisect import bisect_left
 input = sys.stdin.readline
 N = int(input())
 X = list(map(int, input().split()))
 Y = sorted(set(X))
-result = []
-for i in range(len(X)):
-    result.append(bisect_left(Y, X[i]))
-print(*result)
+dic = {Y[i]: i for i in range(len(Y))}
+for i in X:
+    print(dic[i], end=' ')
