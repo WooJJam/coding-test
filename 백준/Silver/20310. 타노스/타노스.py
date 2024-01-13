97667fb234991da766ae9931db21d8c0c2eec220
재민 <111514410+WooJJam@users.noreply.github.com>
2024-01-13 15:50:18 +0900

    [Silver III] Title: 타노스, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -1,11 +1,26 @@
 import sys
 input = sys.stdin.readline
-S = input().rstrip()
+S = list(input().rstrip())
 zero = S.count("0")
 one = len(S) - zero
 answer = ""
-for _ in range(zero//2):
-    answer += "0"
-for _ in range(one//2):
-    answer += "1"
-print(answer)
+count = 0
+for i in range(len(S)-1,-1,-1):
+    if S[i] == "0" and count < (zero//2):
+        S[i] = "-1"
+        count += 1
+    if count == zero:
+        break
+count = 0
+for i in range(len(S)):
+    if S[i] == "1" and count < (one//2):
+        S[i] = "-1"
+        count += 1
+    if count == one:
+        break
+
+for i in range(len(S)):
+    if S[i] == "-1":
+        continue
+    else:
+        print(S[i], end="")
