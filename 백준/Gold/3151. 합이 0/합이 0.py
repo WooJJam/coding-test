a6e29732bad315f6eff5d204339146fee9ececcd
재민 <111514410+WooJJam@users.noreply.github.com>
2024-01-22 14:29:11 +0900

    [Gold IV] Title: 합이 0, Time: 5812 ms, Memory: 112600 KB -BaekjoonHub

---
+++ 백준/Gold/3151. 합이 0/합이 0.py
@@ -0,0 +1,27 @@
+import sys, bisect
+
+input = sys.stdin.readline
+N = int(input())
+arr = list(map(int, input().split()))
+arr.sort()
+answer = 0
+for first in range(N):
+    second = first + 1
+    third = N-1
+    while second < third:
+        result = arr[first] + arr[second] + arr[third]
+        if result > 0:
+            third -= 1
+        elif result < 0:
+            second += 1
+        else:
+            if arr[second] == arr[third]:
+                answer += (third - second)
+            else:
+                third_left = bisect.bisect_left(arr, arr[third])
+                third_right = bisect.bisect_right(arr, arr[third])
+                # print("!!", arr[first], arr[second], arr[third])
+                answer += third_right - third_left
+                # print(arr[second], second, second_right)
+            second += 1
+print(answer)
