5a6ce73d380044b5ac23b45b555d5bcdf88164d9
재민 <111514410+WooJJam@users.noreply.github.com>
2024-01-22 11:39:35 +0900

    [Gold IV] Title: 준표의 조약돌, Time: 384 ms, Memory: 33956 KB, Score: 250 point -BaekjoonHub

--- 백준/Gold/15831. 준표의 조약돌/준표의 조약돌.py
+++ 백준/Gold/15831. 준표의 조약돌/준표의 조약돌.py
@@ -4,24 +4,22 @@ N,B,W = map(int, input().split())
 stone = list(map(str, input().rstrip()))
 end = 0
 b, w = 0, 0
-check = True
 answer = 0
+start = 0
 # B보다 작거나 같게, W보다 같거나 많게
-for start in range(N):
-    while end < N:
+while end < N:
+    if b > B:
+        if stone[start] == "W":
+            w -= 1
+        if stone[start] == "B":
+            b -= 1
+        start += 1
+    else:
         if stone[end] == "B":
-            if b < B:
-                b += 1
-            else:
-                break
+            b += 1
         if stone[end] == "W":
             w += 1
         end += 1
-    if stone[start] == "B":
-        b -= 1
-    else:
-        w -= 1
     if b <= B and w >= W:
         answer = max(answer, end-start)
-
 print(answer)
