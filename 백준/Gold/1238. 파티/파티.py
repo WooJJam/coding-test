ae70cfed7f7af70812bd9de19c043199b268d499
재민 <111514410+WooJJam@users.noreply.github.com>
2024-01-24 14:15:53 +0900

    [Gold III] Title: 파티, Time: 1940 ms, Memory: 34212 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -4,35 +4,29 @@ N, M, X = map(int , input().split())
 INF = int(10e9)
 graph = [[] for _ in range(N+1)]
 
-go_distance = [INF] * (N+1)
-back_distance = [INF] * (N+1)
-
 for _ in range(M):
     s, e, t = map(int, input().split())
     graph[s].append([e,t])
 answer = 0
 
-def dijkstra(start, distance):
+def dijkstra(start,end):
+    distance = [INF] * (N+1)
     q = []
     heapq.heappush(q, (0, start))
     distance[start] = 0
-
     while q:
         dist, now = heapq.heappop(q)
-
         if distance[now] < dist:
             continue
-
         for i in graph[now]:
             cost = dist + i[1]
             if cost < distance[i[0]]:
                 distance[i[0]] = cost
                 heapq.heappush(q, (cost, i[0]))
+    return distance[end]
 
 for i in range(1, N+1):
-    dijkstra(i, go_distance)
-    dijkstra(X, back_distance)
-    answer = max(answer, go_distance[X] + back_distance[i])
-    go_distance = [INF] * (N+1)
-    back_distance = [INF] * (N+1)
+    go = dijkstra(i,X)
+    end = dijkstra(X,i)
+    answer = max(answer, go+end)
 print(answer)
