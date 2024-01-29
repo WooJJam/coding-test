226f664ee2b1971e142324131d5febf79c2498c7
재민 <111514410+WooJJam@users.noreply.github.com>
2024-01-30 02:09:44 +0900

    [Silver II] Title: DNA 비밀번호, Time: 460 ms, Memory: 41152 KB -BaekjoonHub

---
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+S, P = map(int, input().split())
+dna = list(map(str, input().rstrip()))
+A, C, G, T = map(int, input().split())
+count = {}
+count['A'] = 0
+count['C'] = 0
+count['G'] = 0
+count['T'] = 0
+start = 0
+answer = 0
+
+def check(count):
+    global answer
+    if count['A'] >= A and count['C'] >= C and count['G'] >= G and count['T'] >= T:
+        answer += 1
+for i in range(P):
+    count[dna[i]] += 1
+check(count)
+
+for i in range(P, len(dna)):
+    count[dna[start]] -= 1
+    count[dna[i]] += 1
+    start += 1
+    check(count)
+print(answer)
