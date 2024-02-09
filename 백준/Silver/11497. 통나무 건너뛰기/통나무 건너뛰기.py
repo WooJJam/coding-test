3533faff72b34af760140df67ee65b1abd021f48
재민 <111514410+WooJJam@users.noreply.github.com>
2024-02-09 14:41:06 +0900

    [Silver I] Title: 통나무 건너뛰기, Time: 228 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.py
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.py
@@ -1,27 +1,11 @@
 import sys
 input = sys.stdin.readline
-
-def get_level(height):
-    level = 0
-    for i in range(N-1):
-        level = max(abs(height[i] - height[i+1]), level)
-    return level
-answer = []
 T = int(input())
 for _ in range(T):
     N = int(input())
-    height = [0] * N
-    arr = list(map(int, input().split()))
-    arr.sort()
-    index = N//2
-    height[index] = arr[-1]
-    num = 0
-    for i in range(N-2, -1, -1):
-        if num < 0 :
-            num = abs(num) + 1
-        else:
-            num += 1
-            num *= -1
-        index += num
-        height[index] = arr[i]
-    print(get_level(height))
+    tree = list(map(int, input().split()))
+    tree.sort()
+    level = 0
+    for i in range(2, N):
+        level = max(level, abs(tree[i]-tree[i-2]))
+    print(level)
