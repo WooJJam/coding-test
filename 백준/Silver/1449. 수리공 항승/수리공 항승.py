69e23923506c8ca43b6d8fb50277a5a5057d2d54
재민 <111514410+WooJJam@users.noreply.github.com>
2024-02-11 20:40:11 +0900

    [Silver III] Title: 수리공 항승, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1449. 수리공 항승/수리공 항승.py
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.py
@@ -4,13 +4,13 @@ N,L = map(int, input().split())
 pos = list(map(int, input().split()))
 pos.sort(reverse=True)
 answer = 0
-i = 0
-for _ in range(N):
-    for j in range(i+1, N):
-        if pos[i] - pos[j] < L:
-            continue
-        else:
-            i = j
-            answer += 1
-            break
-print(answer+1)
+start = 0
+end = start + 1
+while end < N:
+    if pos[start] - pos[end] < L:
+        end += 1
+        continue
+    else:
+        answer += 1
+        start = end
+print(answer + 1)
