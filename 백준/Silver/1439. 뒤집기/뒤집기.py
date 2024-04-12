00f02f7a3b0aec8d462987d392805a913fb63c3a
재민 <111514410+WooJJam@users.noreply.github.com>
2024-04-12 19:33:10 +0900

    [Silver V] Title: 뒤집기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -1,31 +1,8 @@
 import sys
 input = sys.stdin.readline
-
 num = list(map(str, input().rstrip()))
-zeroCount = 0
-oneCount = 0
-check = num[0]
-if '0' and '1' in num:
-    for i in range(len(num)):
-        if num[i] == '0':
-            if check == '0':
-                continue
-            else :
-                oneCount += 1
-                check = '0'
-        elif num[i] == '1':
-            if check == '1':
-                continue
-            else:
-                zeroCount += 1
-                check = '1'
-
-    if num[-1] == '1':
-        oneCount += 1
-    else:
-        zeroCount += 1
-
-    print(min(zeroCount, oneCount))
-else:
-    print(0)
-    exit(0)
+answer = 0
+for i in range(len(num) - 1):
+    if num[i] != num[i+1]:
+        answer += 1
+print((answer+1)//2)
