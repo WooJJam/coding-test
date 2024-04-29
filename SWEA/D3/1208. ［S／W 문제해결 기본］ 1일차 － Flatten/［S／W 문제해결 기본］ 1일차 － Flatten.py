bd11f49e591defc13e6309bac57dcc3ecad5d6cf
재민 <111514410+WooJJam@users.noreply.github.com>
2024-04-30 00:39:59 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 123 ms, Memory: 48,772 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
@@ -5,19 +5,20 @@ for test_case in range(1, 11):
     count = 0
 
     while count < n:
-        _max = max(boxes)
-        _min = min(boxes)
+        max_index, min_index = 0, 0
+        _max, _min = float('-inf'), float('inf')
 
-        for i in range(100):
-            if boxes[i] == _max:
+        # 최대값과 최소값의 인덱스 찾기
+        for i, box in enumerate(boxes):
+            if box > _max:
+                _max = box
                 max_index = i
-            
-            if boxes[i] == _min:
+            if box < _min:
+                _min = box
                 min_index = i
         
         boxes[max_index] -= 1
         boxes[min_index] += 1
         count += 1
     
-    
-    print(f" #{test_case} {max(boxes) - min(boxes)}")
+    print(f"#{test_case} {max(boxes) - min(boxes)}")
