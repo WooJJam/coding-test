2b3cc482251f3fd76351575f7c02c2ed5b276baf
재민 <111514410+WooJJam@users.noreply.github.com>
2024-05-10 01:46:48 +0900

    [D2] Title: 수도 요금 경쟁 D2 Attack, Time: 103 ms, Memory: 42,628 KB -BaekjoonHub

--- SWEA/D2/1284. 수도 요금 경쟁 D2 Attack/수도 요금 경쟁 D2 Attack.py
+++ SWEA/D2/1284. 수도 요금 경쟁 D2 Attack/수도 요금 경쟁 D2 Attack.py
@@ -1,4 +1,1 @@
-T = int(input())
-for test_case in range(1, T+1):
-    P, Q, R, S, W = map(int, input().split())
-    print(f"#{test_case} {min(W*P , Q if W <= R else Q + (W-R) * S)}")
+for t in range(int(input())):P, Q, R, S, W = map(int, input().split()); print(f"#{t+1} {min(W*P , Q if W <= R else Q + (W-R) * S)}")
