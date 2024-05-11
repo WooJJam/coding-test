9be313a9a03e8ae1d3f447b259d79ea2934805d4
재민 <111514410+WooJJam@users.noreply.github.com>
2024-05-11 15:07:32 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,717 ms, Memory: 81,764 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.py
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.py
@@ -1,21 +1,25 @@
 T = int(input())
 
 def big_dfs(x):
-    visited[x] = True
+    global count
     for i in big_graph[x]:
         if not visited[i]:
             visited[i] = True
+            count += 1
             big_dfs(i)
 
 def small_dfs(x):
+    global count
     for i in small_graph[x]:
         if not visited[i]:
             visited[i] = True
+            count += 1
             small_dfs(i)
 
 for test_case in range(1, T+1):
     N = int(input()) # 사람의 수 (vertex)
     M = int(input()) # 키 비교 수 (edef)
+    count = 0
     big_graph = [[] for _ in range(N+1)]
     small_graph = [[] for _ in range(N+1)]
     answer = 0
@@ -25,13 +29,12 @@ for test_case in range(1, T+1):
         small_graph[b].append(a)
     
     for i in range(1, N+1):
+        count = 1
         visited=[False] * (N+1)
+        visited[i] = True
         big_dfs(i)
         small_dfs(i)
-        for j in range(1, N+1):
-            if not visited[j]:
-                break
-        else:
+        if count == N:
             answer += 1
     
     print(f"#{test_case} {answer}")
