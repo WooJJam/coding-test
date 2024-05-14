5f96f64a63a32839d7559d8574a468d6563f6301
재민 <111514410+WooJJam@users.noreply.github.com>
2024-05-14 16:24:29 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 105 ms, Memory: 43,696 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.py
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.py
@@ -1,4 +1,10 @@
+def solve(x, depth):
+    if depth == M:
+        return x
+    else:
+        return solve(x*N, depth + 1)
+    
 for test_case in range(1, 11):
     _ = int(input())
     N, M = map(int, input().split())
-    print(f"#{test_case} {N**M}")
+    print(f"#{test_case} {solve(N, 1)}")
