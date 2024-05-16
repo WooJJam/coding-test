2ad4a12de48f8af602cc23fca5c266badf52d83c
재민 <111514410+WooJJam@users.noreply.github.com>
2024-05-16 19:30:53 +0900

    [Silver II] Title: 베르트랑 공준, Time: 880 ms, Memory: 35360 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -1,16 +1,19 @@
 def solve():
     global answer
-    for i in range(N+1, 2*N+1):
-        for j in range(2, int((2*N)**(1/2)+1)):
-            if i % j == 0 :
-                break
+    for i in range(2, int((2*N) ** 0.5) + 1):
+        if check[i]:
+            continue
         else:
-            answer += 1
+            for j in range(i+i, 2*N+1, i):
+                # print(i,j)
+                check[j] = True
+
 while True:
     answer = 0
     N = int(input())
     if N == 0:
         break
     else:
+        check = [False] * ((2 * N)+1)
         solve()
-        print(answer)
+        print(check[N+1:].count(False))
