86845e6a03fef8c86faf2ac37a538ef5cdd692d9
WooJJam <111514410+WooJJam@users.noreply.github.com>
2025-02-10 00:36:19 +0900

    [Silver V] Title: 막대기, Time: 120 ms, Memory: 15808 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -6,9 +6,18 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int x = Integer.parseInt(br.readLine());
-		String result = Integer.toBinaryString(x);
-		long count = result.chars().filter(c -> c == '1').count();
-		bw.write((int)count+"\n");
+		int bar = 64;
+		int result = 0;
+        
+		while (x > 0) {
+			if (bar > x) {
+				bar /= 2;
+			} else {
+				x -= bar;
+				result += 1;
+			}
+		}
+		bw.write(result+"\n");
 		bw.flush();
 		bw.close();
 	}
