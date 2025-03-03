c1ecb787efc2b439451ac5763bba677bca8e7410
WooJJam <111514410+WooJJam@users.noreply.github.com>
2025-03-03 21:36:08 +0900

    [Gold IV] Title: 알고스팟, Time: 72 ms, Memory: 35508 KB -BaekjoonHub

---
+++ 백준/Gold/1261. 알고스팟/알고스팟.py
@@ -0,0 +1,41 @@
+import sys, heapq
+
+dy = [-1, 0, 1, 0]
+dx = [0, 1, 0, -1]
+
+def solve(i, j):
+    q = []
+    distance[i][j] = 0
+    heapq.heappush(q, (i, j, 0))
+
+    while q:
+        y, x, cost = heapq.heappop(q)
+        if cost > distance[y][x]: # 현재 비용이 해당 지점의 비용보다 크다면 방문처리
+            continue
+
+        for i in range(4):
+            nextY = dy[i] + y
+            nextX = dx[i] + x
+            dist = cost
+            if (0 <= nextY < M and 0 <= nextX < N): # 범위내에 있다면 이동 가능
+                if miro[nextY][nextX] == 1:
+                    dist += 1
+
+                if distance[nextY][nextX] > dist:
+                    # print(nextY, nextX, cost)
+                    distance[nextY][nextX] = dist
+                    heapq.heappush(q, (nextY, nextX, dist))
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+miro = [] * M
+MAX = int(10e9)
+
+distance = [[MAX for _ in range(N)] for _ in range(M)]
+
+for i in range(M):
+    miro.append(list(map(int, input().rstrip())))
+
+solve(0,0)
+print(distance[M-1][N-1])
