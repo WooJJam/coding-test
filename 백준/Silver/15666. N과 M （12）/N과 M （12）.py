d6d10608c72d197bdb98dd51dd99a14de44466b2
WooJJam <111514410+WooJJam@users.noreply.github.com>
2025-03-08 01:42:33 +0900

    [Silver II] Title: N과 M (12), Time: 1480 ms, Memory: 111404 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.py
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -6,19 +6,12 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 array = list(map(int, input().split()))
 array.sort()
-status = True
 
 answer = []
 
 for number in product(array, repeat = M):
     for j in range(M-1):
-        for k in range(j, M):
-            if number[j] > number[k]:
-                status = False
-                break
-
-        if not status:
-            status = True
+        if number[j] > number[j+1]:
             break
     else:
         if number not in answer:
