f5b979c734f4f3181eac898d69f86fc746d7d21c
WooJJam <111514410+WooJJam@users.noreply.github.com>
2025-04-11 00:02:42 +0900

    [level 2] Title: 타겟 넘버, Time: 201.86 ms, Memory: 168 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    
+    static int answer = 0;
+    
+    public int solution(int[] numbers, int target) {
+
+        return bfs(target, numbers);
+    }
+    
+    static int bfs(int target, int[] numbers) {
+        
+        Queue<int[]> q = new LinkedList<int[]>();
+        
+        q.offer(new int[]{numbers[0], 1});
+        q.offer(new int[]{numbers[0] * -1, 1});
+        
+        while(!q.isEmpty()) {
+            
+            int[] cur = q.poll();
+            int pos = cur[0]; // 4
+            int count = cur[1]; // 1
+            
+            if (count == numbers.length) {
+                if (pos == target) {
+                    answer += 1;
+                }
+            } else {
+                q.offer(new int[]{pos + numbers[count], count + 1});
+                q.offer(new int[]{pos + (numbers[count] * -1), count + 1 });
+                }
+        }
+        
+        return answer;
+    }
+}
