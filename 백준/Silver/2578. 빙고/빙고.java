14b04cbf9e6ad7f2d3be5ae8da131774d98fee7b
WooJJam <111514410+WooJJam@users.noreply.github.com>
2025-05-28 23:43:19 +0900

    [Silver IV] Title: 빙고, Time: 104 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.java
+++ 백준/Silver/2578. 빙고/빙고.java
@@ -36,10 +36,12 @@ public class Main {
 			for (int j = 0; j < 5; j++) {
 				playGame(i, j);
 
-				if (bingo >= 3){
+				if (bingo >= 3) {
 					System.out.println(i * 5 + j + 1);
 					return;
 				}
+
+				bingo = 0;
 			}
 		}
 	}
@@ -51,7 +53,7 @@ public class Main {
 				// 철수의 숫자와 사회자가 부른 숫자가 같은 칸
 				if (game[i][j] == mc[x][y]) {
 					game[i][j] = 0;
-					checkBingo(i, j);
+					checkBingo();
 					return;
 				}
 			}
@@ -59,53 +61,47 @@ public class Main {
 
 	}
 
-	private static void checkBingo(int i, int j) {
-
-		// System.out.println();
-		// System.out.println("i = " + i);
-		// System.out.println("j = " + j);
-		// System.out.println(i * 5 + j);
-		// System.out.println("bingo = " + bingo);
-
-		checkLeftDiagonal(i, j);
-		checkRightDiagonal(i, j);
-		checkVertical(j);
-		checkRow(i);
-
-
-		// if (bingo == 3) {
-		// 	System.out.println("i = " + i);
-		// 	System.out.println("j = " + j);
-		// 	System.out.println(i * 5 + j);
-		// }
+	private static void checkBingo() {
+		checkLeftDiagonal();
+		checkRightDiagonal();
+		checkVertical();
+		checkRow();
 	}
 
-	private static void checkLeftDiagonal(int i,  int j) {
+	private static void checkLeftDiagonal() {
 		int result = 0;
 
 		// 왼쪽 대각선
-		if ((i == 0 && j ==0) || (i == 1 && j ==1) || (i == 2 && j ==2) || (i == 3 && j == 3) || (i == 4 && j ==4)) {
-			for (int k = 0; k < 5; k++) {
-				result += game[k][k];
-			}
+		for (int k = 0; k < 5; k++) {
+			result += game[k][k];
+		}
 
-			if (result == 0) {
-				bingo += 1;
-			}
+		if (result == 0) {
+			bingo += 1;
 		}
 	}
 
-	private static void checkRightDiagonal(int i, int j) {
-
-		int x = 0;
-		int y = 4;
+	private static void checkRightDiagonal() {
 
 		int result = 0;
 
 		// 오른쪽 대각선
-		if ((i == 0 && j == 4) || (i == 1 && j == 3) || (i == 2 && j ==2) || (i == 3 && j == 1) || (i == 4 && j == 0)) {
-			for (int k = 0; k < 5; k++) {
-				result += game[x++][y--];
+		for (int k = 0; k < 5; k++) {
+			result += game[4 - k][k];
+		}
+
+		if (result == 0) {
+			bingo += 1;
+		}
+	}
+
+	private static void checkVertical() {
+
+		for (int i = 0; i < 5; i++) {
+			int result = 0;
+
+			for (int j = 0; j < 5; j++) {
+				result += game[i][j];
 			}
 
 			if (result == 0) {
@@ -114,29 +110,20 @@ public class Main {
 		}
 	}
 
-	private static void checkVertical(int j) {
+	private static void checkRow() {
 
-		int result = 0;
+		for (int i = 0; i < 5; i++) {
 
-		for (int k = 0; k < 5; k++) {
-			result += game[k][j];
+			int result = 0;
+
+			for (int j = 0; j < 5; j++) {
+				result += game[j][i];
+			}
+
+			if (result == 0) {
+				bingo += 1;
+			}
 		}
 
-		if (result == 0) {
-			bingo += 1;
-		}
-	}
-
-	private static void checkRow(int i) {
-
-		int result = 0;
-
-		for (int k = 0; k < 5; k++) {
-			result += game[i][k];
-		}
-
-		if (result == 0) {
-			bingo += 1;
-		}
 	}
 }
