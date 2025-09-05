063a2c718039ffb4160fde629bb357a573f84419
WooJJam <111514410+WooJJam@users.noreply.github.com>
2025-09-05 16:01:50 +0900

    [Silver I] Title: 절댓값 힙, Time: 436 ms, Memory: 26472 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,42 +1,56 @@
-import java.security.Key;
-import java.util.*;
-import java.io.*;
-
-import javax.lang.model.element.Name;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	static PriorityQueue<Integer> pq = new PriorityQueue<>((x, y) -> {
-		int absX = Math.abs(x);
-		int absY = Math.abs(y);
-
-		if (absX == absY) {
-			return x < y ? -1 : 1;
-		}
-
-		return absX < absY ? -1 : 1;
-	});
+	private static PriorityQueue<Integer> pq;
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
 		int N = Integer.parseInt(br.readLine());
+		int[] input = new int[N];
 
 		for (int i = 0; i < N; i++) {
-			int x = Integer.parseInt(br.readLine());
-			if (x == 0) {
-				if (pq.size() == 0) {
-					bw.write("0\n");
-				} else {
-					bw.write(pq.poll() + "\n");
-				}
-			} else {
-				pq.add(x);
-			}
+			input[i] = Integer.parseInt(br.readLine());
 		}
 
-		bw.flush();
-		bw.close();
+		pq = new PriorityQueue<>(
+			(a, b) -> {
+				int compareNum = Integer.compare(Math.abs(a), Math.abs(b));
+
+				if (compareNum == 0) {
+					return Integer.compare(a, b);
+				}
+
+				return compareNum;
+			});
+
+		solve(input);
+	}
+
+	private static void solve(int[] input) {
+
+		for (int i = 0; i < input.length; i++) {
+			if (input[i] == 0) {
+				if (pq.size() > 0) {
+					System.out.println(pq.poll());
+				} else {
+					System.out.println(0);
+				}
+			} else {
+				pq.add(input[i]);
+			}
+		}
 	}
 }
