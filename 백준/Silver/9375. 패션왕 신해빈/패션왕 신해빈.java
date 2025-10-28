92adaab1307e12537e8b9bc9fd93f3e02820f343
WooJJam <111514410+WooJJam@users.noreply.github.com>
2025-10-28 17:13:21 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 108 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -1,29 +1,47 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main { ;
+
 	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
 
 		int T = Integer.parseInt(br.readLine());
-		int N = 0;
 		for (int i = 0; i < T; i++) {
-			HashMap<String, Integer> map = new HashMap<>();
-			N = Integer.parseInt(br.readLine());
+			int N = Integer.parseInt(br.readLine());
+			HashMap<String, Integer> clothes = new HashMap<>();
+
 			for (int j = 0; j < N; j++) {
-				String[] clothes = br.readLine().split(" ");
-				map.put(clothes[1], map.getOrDefault(clothes[1], 0) + 1);
+				st = new StringTokenizer(br.readLine());
+				String item = st.nextToken();
+				String category = st.nextToken();
+				clothes.put(category, clothes.getOrDefault(category, 0) + 1);
 			}
-			System.out.println(solve(map));
+			
+			solve(clothes);
 		}
+
+
 	}
 
-	static int solve(HashMap<String, Integer> map) {
+	private static void solve(HashMap<String, Integer> clothes) {
 		int answer = 1;
-		for (int value: map.values()) {
-			answer *= (value + 1);
+		
+		for (String key : clothes.keySet()) {
+			answer *= clothes.get(key) + 1;
 		}
-		return answer - 1;
+
+		System.out.println(answer - 1);
 	}
 }
