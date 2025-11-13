fcf89896dead73b948398ee26e13f32a47a4cdc1
WooJJam <111514410+WooJJam@users.noreply.github.com>
2025-11-13 17:37:53 +0900

    [Silver III] Title: 파도반 수열, Time: 104 ms, Memory: 14452 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,33 +1,41 @@
-import java.util.*;
-import java.io.*;
-import java.lang.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
+
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
 
 		int T = Integer.parseInt(br.readLine());
 
-		long[] P = new long[101];
-
-		P[0] = 0;
-		P[1] = 1;
-		P[2] = 1;
-		P[3] = 1;
-		P[4] = 2;
-
-		for (int i = 5; i <= 100; i++) {
-			P[i] = P[i-1] + P[i-5];
-		}
-
 		for (int i = 0; i < T; i++) {
 			int N = Integer.parseInt(br.readLine());
-			bw.write(P[N]+"\n");
+
+			dynamic(N);
 		}
 
-		bw.flush();
-		bw.close();
+	}
+
+	private static void dynamic(int N) {
+
+		long[] dp = new long[101];
+
+		dp[1] = 1;
+		dp[2] = 1;
+
+		for (int i = 3; i <= N; i++) {
+			dp[i] = dp[i-3] + dp[i-2];
+		}
+
+		System.out.println(dp[N]);
 	}
 }
