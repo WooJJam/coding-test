90ecb3c5b774f711ad6988d098cbac46bb0fd19a
WooJJam <111514410+WooJJam@users.noreply.github.com>
2025-12-07 01:16:34 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -0,0 +1,6 @@
+select member_id, member_name, gender, date_format(date_of_birth, '%Y-%m-%d') date_of_birth
+from member_profile
+where month(date_of_birth) = 3
+and tlno != 'null'
+and gender = 'W'
+order by member_id asc;
