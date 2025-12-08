1bbe09f580ee4c923e2ffad000b6690eb07e2614
WooJJam <111514410+WooJJam@users.noreply.github.com>
2025-12-08 16:53:28 +0900

    [level 2] Title: 부모의 형질을 모두 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
@@ -0,0 +1,5 @@
+select ed1.id, ed1.genotype, ed2.genotype as parent_genotype
+from ecoli_data ed1
+join ecoli_data ed2 on ed1.parent_id = ed2.id
+where ed1.genotype & ed2.genotype = ed2.genotype
+order by ed1.id ASC;
