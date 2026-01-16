66091f219ec9cf922d0bf31b9c4389f46e2bd88c
WooJJam <111514410+WooJJam@users.noreply.github.com>
2026-01-16 15:00:17 +0900

    [Silver IV] Title: 암기왕, Time: 1504 ms, Memory: 280196 KB -BaekjoonHub

---
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
+		int T = Integer.parseInt(br.readLine());
+		for (int i = 0; i < T; i++) {
+			int N = Integer.parseInt(br.readLine());
+			HashSet<Integer> note = new HashSet<>();
+
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				note.add(Integer.parseInt(st.nextToken()));
+			}
+
+			int M = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				int number = Integer.parseInt(st.nextToken());
+				if (note.contains(number)) {
+					bw.write("1\n");
+				} else {
+					bw.write("0\n");
+				}
+			}
+		}
+		bw.flush();
+		bw.close();
+	}
+}
