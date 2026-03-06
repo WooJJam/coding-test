986654b903a17b78c8d343e87d67e8c3bd87bf29
WooJJam <111514410+WooJJam@users.noreply.github.com>
2026-03-06 14:40:11 +0900

    [Silver III] Title: 2×n 타일링, Time: 108 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+		int[] dp = new int[1_001];
+
+		dp[0] = 0;
+		dp[1] = 1;
+		dp[2] = 2;
+
+		for (int i = 3; i <= N; i++) {
+			dp[i] = (dp[i-1] + dp[i-2]) % 10_007;
+		}
+
+		System.out.println(dp[N]);
+	}
+}
