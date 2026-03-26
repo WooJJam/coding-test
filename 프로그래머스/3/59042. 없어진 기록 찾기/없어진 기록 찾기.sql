e1d9053c682abc4f65665823d4fcff2b881d06fd
WooJJam <111514410+WooJJam@users.noreply.github.com>
2026-03-26 19:18:06 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,6 +1,9 @@
 -- 코드를 입력하세요
 SELECT ao.ANIMAL_ID, ao.NAME
-from animal_ins ai
-right join animal_outs ao on ai.animal_id = ao.animal_id
-where ai.animal_id is null
-order by ao.ANIMAL_ID;
+from animal_outs ao
+where not exists (
+    select 1
+    from animal_ins ai
+    where ai.animal_id = ao.animal_id
+)
+order by ao.animal_id 
